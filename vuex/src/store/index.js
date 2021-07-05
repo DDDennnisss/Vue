fd82a79a19a8a2dbf8d5d0a5b0ae9b1a1769ea2a
Mingwei Deng <34043558+DDDennnisss@users.noreply.github.com>
2021-07-05 10:36:18 -0400

    notes timeline updated

--- vuex/src/store/index.js
+++ vuex/src/store/index.js
@@ -5,7 +5,13 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    counter: 1000
+    counter: 1000,
+    student: [
+      { id: 100, name: "mike", age: 10 },
+      { id: 101, name: "jack", age: 15 },
+      { id: 102, name: "dennis", age: 30 },
+      { id: 103, name: "jessica", age: 21 }
+    ]
   },
   mutations: {
     increment(state) { //对应上面state对象
@@ -19,7 +25,14 @@ const store = new Vuex.Store({
 
   },
   getters: {
-
+    more20stu() {
+      return state.student.filter(ele => ele.age > 20);
+    },
+    moreAgeStu(state) {
+      return function (num) {
+        return state.student.filter(ele => ele.age > num);
+      }
+    }
   },
   modules: {
 
